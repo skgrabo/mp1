f197462c3817e68f689997de1becc53d8ac06c15
Sara <skgrabo@yahoo.com>
2026-09-11 15:01:58 -0500

    added main sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,40 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+} 
+
+window.onscroll = function() {scroll()};
+function scroll() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "10px 10px";
+    //srink text too
+  } else {
+    document.getElementById("navbar").style.padding = "60px 10px";
+    //make texet bigger again
+  }
+} 
